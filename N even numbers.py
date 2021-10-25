79b2d14d7fbaeb436c0435a5fd6e7349a6fe964b
Bharathidasan204 <77656801+Bharathidasan204@users.noreply.github.com>
2021-10-25 17:32:55 +0530

    Create N even numbers.py

---
+++ N even numbers.py
@@ -0,0 +1,6 @@
+# Python Program to Print Even Numbers from 1 to N
+
+maximum = int(input(" Please Enter the Maximum Value : "))
+
+for number in range(2, maximum+1, 2):
+    print("{0}".format(number))
